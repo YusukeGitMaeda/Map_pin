d49e7f1b35a9a56a53a1d8c2fb960a1406958a59
YusukeMaeda <yusukebask@yahoo.co.jp>
2015-06-12 22:19:13 +0900

    Initial Commit

---
+++ Map_pin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map_pin
+//
+//  Created by 前田雄亮 on 2015/06/12.
+//  Copyright (c) 2015年 前田雄亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
